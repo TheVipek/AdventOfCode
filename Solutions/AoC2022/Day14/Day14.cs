2c05385201e7d4f03390b4c5efb510eb2a6d372e
Vipek <thevipek0552@gmail.com>
2023-01-07 18:05:13 +0100

    Day 14 part 2

--- Solutions/AoC2022/Day14/Day14.cs
+++ Solutions/AoC2022/Day14/Day14.cs
@@ -1,41 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading;
+
 
 namespace AoC2022.Day14
 {
+    public class Position
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Position(int x,int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void SetPositionX(int x) => X = x;
+        public void SetPositionY(int y) => Y = y;
+        public void SetPosition(Position position)
+        {
+            X = position.X;
+            Y = position.Y;
+        }
+    }
+    
     public class Sand
     {
-        public int[] Position { get; private set; } 
+        public Position Position{ get; private set; } 
+        public Position Down { get { return new Position(Position.X, Position.Y + 1); } } 
+        public Position DownLeft { get { return new Position(Position.X - 1, Position.Y + 1); } }
+        public Position DownRight { get { return new Position(Position.X + 1, Position.Y + 1); } }
+
         public bool IsRested { get; set; } = false;
         
-        public Sand(int[] position)
+        public Sand(Position position)
         {
             Position = position;
         }
-        public void SetPositionX(int x) => Position[0] = x;
-        public void SetPositionY(int y) => Position[1] = y;  
-        public void SetPosition(int[] newPosition) => Position = newPosition;
+        
     }
     public class MapStructure
     {
-        public List<int[]> elementsOnMap = new List<int[]>();
-        public List<Sand> sandOnMap = new List<Sand>();
+        public Dictionary<string, Position> elementsOnMap = new Dictionary<string, Position>();
+        public HashSet<Sand> sandOnMap = new HashSet<Sand>();
 
-        // [0] lowest range, [1] highest range
-        public int originSandX { get; } = 500;
-        public int originSandY { get; } = 0;
-        public int[] StructureRangeX { get; private set; }
+        public Position OriginSand { get; private set; } = new Position(500, 0);
+        public Position StructureRangeX { get; private set; }
+        public int MinimumYPoint { get; private set; }
 
         public MapStructure() { }
-        public void GeneratingProcess(int elementPositionX, int elementPositionY)
+        public void GeneratingProcess(Position pos)
         {
-            if (CanGenerate(elementPositionX,elementPositionY)) 
+            if (CanGenerate(pos)) 
             { 
-                int[] elementPosition = new int[2] { elementPositionX, elementPositionY };
-                elementsOnMap.Add(elementPosition);
+                elementsOnMap.Add($"{pos.X}:{pos.Y}",pos);
                // Console.WriteLine($"Generation at: {elementPosition[0]}:{elementPosition[1]}");
             } 
         }
@@ -45,37 +63,67 @@ namespace AoC2022.Day14
         /// <param name="xPos"></param>
         /// <param name="yPos"></param>
         /// <returns></returns>
-        public bool CanGenerate(int xPos, int yPos) => !elementsOnMap.Any(x => x[0] == xPos && x[1] == yPos);
+        public bool CanGenerate(Position pos)
+        {
+
+            if (!elementsOnMap.ContainsKey($"{pos.X}:{pos.Y}"))
+            {
+                return true;
+            }
+            return false;
+          
+        }
         /// <summary>
         /// Smaller x position and the biggest x
         /// </summary>
-        public void SetAbyssPositions()
+        public void SetAbyssPositions(int minRangeX = 0 ,int maxRangeX = 0)
         {
-            int[] abyss = new int[2] { elementsOnMap.Min(x => x[0]), elementsOnMap.Max(x => x[0]) };
-            //Console.WriteLine($"Abyss at: {abyss[0]} , {abyss[1]}");
-            StructureRangeX = abyss;
+            if(minRangeX != 0 && maxRangeX != 0)
+            {
+                StructureRangeX = new Position(minRangeX,maxRangeX);
+                return;
+            }
+            StructureRangeX = new Position(elementsOnMap.Min(pos => pos.Value.X), elementsOnMap.Max(pos => pos.Value.X));
         }
+        /// <summary>
+        /// Set minimum Y point for structure
+        /// We're using Max method,beacuse the lower we go ,the lower value gets
+        /// </summary>
+        /// <param name="additionalDown">How much lower we want go from minimum Y structure point</param>
+        public void SetMinimumYPoint(int additionalDown = 0) 
+        { 
+            
+            MinimumYPoint = elementsOnMap.Max(element => element.Value.Y) + additionalDown ;
+            //we need to generate additional layer
+            if(additionalDown > 0)
+            {
+                FillBeetwen(new Position(StructureRangeX.X, MinimumYPoint),new Position(StructureRangeX.Y, MinimumYPoint));
+            }
+            
+        }
+        
+            
         /// <summary>
         /// Get line of scan and applies it to mapStructure
         /// </summary>
         /// <param name="scan"></param>
         public void SetPartOfStructure(string[] scan)
         {
-            int[] lastPosition = null;
+            Position lastPosition = null;
             //i+=2 beacuse we want to catch x and y and they're on different indexes
             for (int i = 0; i < scan.Length; i += 2)
             {
-                int[] currentPosition = new int[2]
-                {
+                Position currentPosition = new Position(
                     int.Parse(scan[i]),
-                    int.Parse(scan[i+1])
-                };
+                    int.Parse(scan[i + 1])
+                    );
+
                 if (lastPosition == null)
                 {
 
                     lastPosition = currentPosition;
                     //Console.Write($"{lastPosition[0]}:{lastPosition[1]} - ...\n");
-                    GeneratingProcess(lastPosition[0], lastPosition[1]);
+                    GeneratingProcess(lastPosition);
                     continue;
                 }
                 //Console.Write($"lastPosition:{lastPosition[0]}:{lastPosition[1]} - {currentPosition[0]}:{currentPosition[1]} \n");
@@ -90,12 +138,12 @@ namespace AoC2022.Day14
         /// </summary>
         /// <param name="lastPosition"></param>
         /// <param name="currentPosition"></param>
-        public void FillBeetwen(int[] lastPosition, int[] currentPosition)
+        public void FillBeetwen(Position lastPosition, Position currentPosition)
         {
 
             //difference between last and current pos
-            int differenceInPositionX = lastPosition[0] - currentPosition[0];
-            int differenceInPositionY = lastPosition[1] - currentPosition[1];
+            int differenceInPositionX = lastPosition.X - currentPosition.X;
+            int differenceInPositionY = lastPosition.Y - currentPosition.Y;
             //Console.WriteLine($"DifferenceX:{differenceInPositionX} DifferenceY:{differenceInPositionY}");
 
 
@@ -104,7 +152,7 @@ namespace AoC2022.Day14
             {
                 while (differenceInPositionX > 0)
                 {
-                    GeneratingProcess(lastPosition[0] - differenceInPositionX, lastPosition[1]);
+                    GeneratingProcess(new Position(lastPosition.X - differenceInPositionX, lastPosition.Y));
                     differenceInPositionX--;
                 }
             }
@@ -113,7 +161,7 @@ namespace AoC2022.Day14
             {
                 while (differenceInPositionX < 0)
                 {
-                    GeneratingProcess(lastPosition[0] - differenceInPositionX, lastPosition[1]);
+                    GeneratingProcess(new Position(lastPosition.X - differenceInPositionX, lastPosition.Y));
                     differenceInPositionX++;
 
                 }
@@ -123,7 +171,7 @@ namespace AoC2022.Day14
             {
                 while (differenceInPositionY > 0)
                 {
-                    GeneratingProcess(lastPosition[0], lastPosition[1] - differenceInPositionY);
+                    GeneratingProcess(new Position(lastPosition.X, lastPosition.Y - differenceInPositionY));
                     differenceInPositionY--;
 
                 }
@@ -133,7 +181,7 @@ namespace AoC2022.Day14
             {
                 while (differenceInPositionY < 0)
                 {
-                    GeneratingProcess(lastPosition[0], lastPosition[1] - differenceInPositionY);
+                    GeneratingProcess(new Position(lastPosition.X, lastPosition.Y - differenceInPositionY));
                     differenceInPositionY++;
 
                 }
@@ -144,15 +192,16 @@ namespace AoC2022.Day14
         //
         public Sand SpawnSand()
         {
-            Sand sand = new(new int[2] {originSandX,originSandY});
+            Sand sand = new(new Position(OriginSand.X,OriginSand.Y));
             //Console.WriteLine($"Sand base position:{sand.Position[0]}:{sand.Position[1]}");
             MoveSand(ref sand);
             //Which means that its in rest state
             if (sand != null && sand.IsRested == true)
             {
+                //Console.WriteLine($"Sand rested position:[{sand.Position[0]}:{sand.Position[1]}");
                // Console.WriteLine("Sand is rest-stated");
                 sandOnMap.Add(sand);
-                elementsOnMap.Add(sand.Position);
+                elementsOnMap.Add($"{sand.Position.X}:{sand.Position.Y}",sand.Position);
 
                 return sand;
 
@@ -162,10 +211,10 @@ namespace AoC2022.Day14
 
         public object MoveSand(ref Sand sand)
         {
-            //Console.WriteLine($"Sand is trying to achieve new position:[{positionMoveTo[0]}:{positionMoveTo[1]}]");
+            //Console.WriteLine($"Sand is trying to achieve new position:[{sand.Position.X}:{sand.Position.Y}");
 
             //Check if sand position isn't out of 'structure bounds'
-            if (sand.Position[0] < StructureRangeX[0] || sand.Position[0] > StructureRangeX[1])
+            if ( (sand.Position.X < StructureRangeX.X || sand.Position.X > StructureRangeX.Y))
             {
                 //Console.WriteLine($"Sand is falling into abyss due to new position [{sand.Position[0]}:{sand.Position[1]}]" +
                 //    $"Abyss range: lower than: {StructureRangeX[0]},higher than:{StructureRangeX[1]}");
@@ -173,25 +222,26 @@ namespace AoC2022.Day14
                 return null;
             }
             //check if we can move down
-            if (CanGenerate(sand.Position[0], sand.Position[1] + 1))
+            if (CanGenerate(sand.Down))
             {
-                sand.SetPositionY(sand.Position[1] + 1);
+                //Console.WriteLine($"Down {sand.Down.X}:{sand.Down.Y}");
+                sand.Position.SetPosition(sand.Down);
                 return MoveSand(ref sand);
             }
             //check if we can move down left
-            if (CanGenerate(sand.Position[0] - 1, sand.Position[1] + 1))
+            if (CanGenerate(sand.DownLeft))
             {
-                sand.SetPositionX(sand.Position[0] - 1);
-                sand.SetPositionY(sand.Position[1] + 1);
+                //Console.WriteLine($"DownLeft {sand.DownLeft.X}:{sand.DownLeft.Y}");
 
+                sand.Position.SetPosition(sand.DownLeft);
                 return MoveSand(ref sand);
             }
             //check if we can move down right
-            if (CanGenerate(sand.Position[0] + 1, sand.Position[1] + 1))
+            if (CanGenerate(sand.DownRight))
             {
-                sand.SetPositionX(sand.Position[0] + 1);
-                sand.SetPositionY(sand.Position[1] + 1);
+                //Console.WriteLine($"DownRight {sand.DownRight.X}:{sand.DownRight.Y}");
 
+                sand.Position.SetPosition(sand.DownRight);
                 return MoveSand(ref sand);
             }
             sand.IsRested = true;
@@ -210,22 +260,47 @@ namespace AoC2022.Day14
                 mapStructure.SetPartOfStructure(scan);
             }
             mapStructure.SetAbyssPositions();
+            mapStructure.SetMinimumYPoint();
             while (true)
             {
                 Sand sandSpawned = mapStructure.SpawnSand();
-                if (sandSpawned == null || sandSpawned.IsRested == false) break;
+                if (sandSpawned == null || sandSpawned.IsRested == false)
+                {
+                    break;
+                }
             }
             //foreach (var item in mapStructure.sandOnMap)
             //{
             //    Console.WriteLine($"{item.Position[0]}:{item.Position[1]}");
             //}
-           // Console.WriteLine(mapStructure.sandOnMap.Count);
+            // Console.WriteLine(mapStructure.sandOnMap.Count);
 
             return mapStructure.sandOnMap.Count.ToString();
 
         }
         public override string SecondPart() 
         {
+            MapStructure mapStructure = new MapStructure();
+            for (int i = 0; i < inputContent.Length; i++)
+            {
+                string[] scan = inputContent[i].Split(new char[] { ',', '-', '>', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                mapStructure.SetPartOfStructure(scan);
+            }
+            mapStructure.SetAbyssPositions(-1000, 1000);
+            mapStructure.SetMinimumYPoint(2);
+            while (true)
+            {
+                Sand sandSpawned = mapStructure.SpawnSand();
+                if(sandSpawned.IsRested == true)
+                {
+                    if (sandSpawned.Position.X == 500 && sandSpawned.Position.Y == 0)
+                    {
+                        break;
+                    }
+
+                }
+            }
+            return mapStructure.sandOnMap.Count.ToString();
             throw new NotImplementedException();
         }
        
