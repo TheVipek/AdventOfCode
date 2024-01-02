8a3792642708910cf4663ef0e9ed0675d06dffd0
TheVipek <thevipek0552@gmail.com>
2024-01-02 22:29:53 +0100

    d7 p2

--- Solutions/Lua/Solution/Day7/Day7.lua
+++ Solutions/Lua/Solution/Day7/Day7.lua
@@ -5,56 +5,30 @@ local solutionCreatorModule = require "solutionCreator";
 
 -- Combinations are amount of the same card needed in hand
 local HandKindsCombinations <const> = {
-    FiveOfAKind = { order = 6, combination = { 5 } },
-    FourOfAKind = { order = 5, combination = { 4, 1 } },
-    FullHouse = { order = 4, combination = { 3, 2 } },
-    ThreeOfAKind = { order = 3, combination = { 3, 1, 1 } },
-    TwoPair = { order = 2, combination = { 2, 2, 1 } },
-    OnePair = { order = 1, combination = { 2, 1, 1, 1 } },
-    HighCard = { order = 0, combination = { 1, 1, 1, 1, 1 } }
+    { name = "FiveOfAKind",  order = 6, combination = { 5 } },
+    { name = "FourOfAKind",  order = 5, combination = { 4, 1 } },
+    { name = "FullHouse",    order = 4, combination = { 3, 2 } },
+    { name = "ThreeOfAKind", order = 3, combination = { 3, 1, 1 } },
+    { name = "TwoPair",      order = 2, combination = { 2, 2, 1 } },
+    { name = "OnePair",      order = 1, combination = { 2, 1, 1, 1 } },
+    { name = "HighCard",     order = 0, combination = { 1, 1, 1, 1, 1 } }
 }
-local CardStrength <const> =
-{
-    ['A'] = 13,
-    ['K'] = 12,
-    ['Q'] = 11,
-    ['J'] = 10,
-    ['T'] = 9,
-    ['9'] = 8,
-    ['8'] = 7,
-    ['7'] = 6,
-    ['6'] = 5,
-    ['5'] = 4,
-    ['4'] = 3,
-    ['3'] = 2,
-    ['2'] = 1
-};
+
 local HAND_SIZE <const> = 5;
 
 Hand = {}
 
-function Hand:New()
-    local o = {
+---@param includeJokers boolean
+function Hand:New(includeJokers)
+    local handObj = {
         bidAmount = 0,
         handKind = -1,
         handCards = "",
+        includeJokers = includeJokers,
     };
 
-    local function CalculateHandKind()
-        local dict = {};
-        for i = 1, #o.handCards do
-            local c = o.handCards:sub(i, i);
-            dict[c] = (dict[c] or 0) + 1;
-        end
-
-
-        local sortedArr = {};
-        for k, v in pairs(dict) do
-            sortedArr[#sortedArr + 1] = { key = k, count = v };
-        end
-        table.sort(sortedArr, function(a, b) return a.count > b.count end);
-
-        for _, comb in pairs(HandKindsCombinations) do
+    local function EvaluateHandWithoutJokers(sortedArr)
+        for _, comb in ipairs(HandKindsCombinations) do
             local correctCombination = false;
             if (#comb.combination == #sortedArr) then
                 for idx, combinationVal in ipairs(comb.combination) do
@@ -66,31 +40,100 @@ function Hand:New()
                     end
                 end
                 if correctCombination then
-                    o.handKind = comb.order;
+                    handObj.handKind = comb.order;
                     return;
                 end
             end
         end
     end
 
-    o.DetermineHandKind = CalculateHandKind;
+    local function EvaluateHandWithJokers(arrWithoutJokers, jokersCount)
+        local jokersCount = jokersCount;
+        local arrWithoutJokers = arrWithoutJokers;
 
-    setmetatable(o, { __index = Hand });
+        if jokersCount == HAND_SIZE then
+            handObj.handKind = HandKindsCombinations[1].order;
+            return;
+        end
 
-    return o;
+        for _, comb in ipairs(HandKindsCombinations) do
+            local correctCombination = false;
+
+            if #arrWithoutJokers == #comb.combination then
+                for idx, combinationVal in ipairs(comb.combination) do
+                    if combinationVal == (arrWithoutJokers[idx].count + jokersCount) then
+                        correctCombination = true;
+                    elseif combinationVal == arrWithoutJokers[idx].count then
+                        correctCombination = true;
+                    else
+                        correctCombination = false;
+                        break;
+                    end
+                end
+
+                if correctCombination then
+                    handObj.handKind = comb.order;
+                    return;
+                end
+            end
+        end
+    end
+
+    local function CalculateHandKind()
+        local dict = {};
+
+        for i = 1, #handObj.handCards do
+            local c = handObj.handCards:sub(i, i);
+            dict[c] = (dict[c] or 0) + 1;
+        end
+        local sortedArr = {};
+        for k, v in pairs(dict) do
+            sortedArr[#sortedArr + 1] = { key = k, count = v };
+        end
+
+        table.sort(sortedArr,
+            function(a, b)
+                return a.count > b.count
+            end);
+
+        if includeJokers then
+            local jokersCount = 0;
+            local arrWithoutJokers = {};
+            for i = 1, #sortedArr do
+                if sortedArr[i].key == "J" then
+                    jokersCount = sortedArr[i].count;
+                else
+                    arrWithoutJokers[#arrWithoutJokers + 1] = { key = sortedArr[i].key, count = sortedArr[i].count };
+                end
+            end
+
+            EvaluateHandWithJokers(arrWithoutJokers, jokersCount);
+        else
+            EvaluateHandWithoutJokers(sortedArr);
+        end
+    end
+
+    handObj.CalculateHandKind = CalculateHandKind;
+
+    setmetatable(handObj, { __index = Hand });
+
+    return handObj;
 end
 
 function Hand:InitializeHand(handCards)
     self.handCards = handCards;
-    self.DetermineHandKind();
+    self.CalculateHandKind();
 end
 
 function Hand:InitializeBid(bid)
     self.bidAmount = bid;
 end
 
+-- region Sorting Algorithms
+
 ---@param hands table
-local function SortHandByStrength_BubbleSort(hands)
+---@param cardsStrength table key-value
+local function SortHandByStrength_BubbleSort(hands, cardsStrength)
     local n = #hands;
     local swapped;
 
@@ -105,8 +148,8 @@ local function SortHandByStrength_BubbleSort(hands)
                 swapped = true;
             elseif leftElement.handKind == rightElement.handKind then
                 for k = 1, HAND_SIZE do
-                    local leftElementStrength = CardStrength[string.sub(leftElement.handCards, k, k)];
-                    local rightElementStrength = CardStrength[string.sub(rightElement.handCards, k, k)];
+                    local leftElementStrength = cardsStrength[string.sub(leftElement.handCards, k, k)];
+                    local rightElementStrength = cardsStrength[string.sub(rightElement.handCards, k, k)];
                     if leftElementStrength > rightElementStrength then
                         hands[i], hands[i + 1] = hands[i + 1], hands[i];
                         swapped = true;
@@ -121,7 +164,9 @@ local function SortHandByStrength_BubbleSort(hands)
         n = n - 1;
     until not swapped
 end
-local function Partition(arr, low, high)
+
+---@param cardsStrength table key-value
+local function Partition(arr, low, high, cardsStrength)
     local pivot = arr[high];
     local i = low - 1;
     local shouldSwap;
@@ -131,8 +176,8 @@ local function Partition(arr, low, high)
             shouldSwap = true;
         elseif arr[j].handKind == pivot.handKind then
             for k = 1, HAND_SIZE do
-                local pivotStrength = CardStrength[string.sub(pivot.handCards, k, k)];
-                local elementStrength = CardStrength[string.sub(arr[j].handCards, k, k)];
+                local pivotStrength = cardsStrength[string.sub(pivot.handCards, k, k)];
+                local elementStrength = cardsStrength[string.sub(arr[j].handCards, k, k)];
                 if pivotStrength < elementStrength then
                     break;
                 elseif pivotStrength > elementStrength then
@@ -149,15 +194,17 @@ local function Partition(arr, low, high)
     arr[i + 1], arr[high] = arr[high], arr[i + 1];
     return i + 1;
 end
-local function SortByHandStrength_QuickSort(arr, low, high)
+---@param cardsStrength table key-value
+local function SortByHandStrength_QuickSort(arr, low, high, cardsStrength)
     if high <= low then return end;
 
-    local pivotIdx = Partition(arr, low, high);
-    SortByHandStrength_QuickSort(arr, low, pivotIdx - 1);
-    SortByHandStrength_QuickSort(arr, pivotIdx + 1, high);
+    local pivotIdx = Partition(arr, low, high, cardsStrength);
+    SortByHandStrength_QuickSort(arr, low, pivotIdx - 1, cardsStrength);
+    SortByHandStrength_QuickSort(arr, pivotIdx + 1, high, cardsStrength);
 end
 
-local function Merge(leftArr, rightArr, arr)
+---@param cardsStrength table key-value
+local function Merge(leftArr, rightArr, arr, cardsStrength)
     local leftIndex, rightIndex, arrIndex = 1, 1, 1;
     local leftSize = math.floor(#arr / 2);
     local rightSize = #arr - leftSize;
@@ -165,20 +212,20 @@ local function Merge(leftArr, rightArr, arr)
         local leftElement = leftArr[leftIndex];
         local rightElement = rightArr[rightIndex];
 
-        if (leftElement.handKind < rightElement.handKind) then
-            arr[arrIndex] = leftArr[leftIndex];
-            leftIndex = leftIndex + 1;
+        if (leftElement.handKind > rightElement.handKind) then
+            arr[arrIndex] = rightArr[rightIndex];
+            rightIndex = rightIndex + 1;
             arrIndex = arrIndex + 1;
         elseif (leftElement.handKind == rightElement.handKind) then
             for k = 1, HAND_SIZE do
-                local leftStrength = CardStrength[string.sub(leftElement.handCards, k, k)];
-                local rightStrength = CardStrength[string.sub(rightElement.handCards, k, k)];
+                local leftStrength = cardsStrength[string.sub(leftElement.handCards, k, k)];
+                local rightStrength = cardsStrength[string.sub(rightElement.handCards, k, k)];
                 if leftStrength < rightStrength then
                     arr[arrIndex] = leftArr[leftIndex];
                     leftIndex = leftIndex + 1;
                     arrIndex = arrIndex + 1;
                     break;
-                elseif leftStrength >= rightStrength then
+                elseif leftStrength > rightStrength then
                     arr[arrIndex] = rightArr[rightIndex];
                     rightIndex = rightIndex + 1;
                     arrIndex = arrIndex + 1;
@@ -186,8 +233,8 @@ local function Merge(leftArr, rightArr, arr)
                 end
             end
         else
-            arr[arrIndex] = rightArr[rightIndex];
-            rightIndex = rightIndex + 1;
+            arr[arrIndex] = leftArr[leftIndex];
+            leftIndex = leftIndex + 1;
             arrIndex = arrIndex + 1;
         end
     end
@@ -207,7 +254,8 @@ local function Merge(leftArr, rightArr, arr)
     end
 end
 
-local function SortByHandStrength_MergeSort(arr)
+---@param cardsStrength table key-value
+local function SortByHandStrength_MergeSort(arr, cardsStrength)
     local len = #arr;
     if len <= 1 then
         return;
@@ -223,13 +271,13 @@ local function SortByHandStrength_MergeSort(arr)
             rArr[#rArr + 1] = arr[i];
         end
     end
-    SortByHandStrength_MergeSort(lArr);
-    SortByHandStrength_MergeSort(rArr);
+    SortByHandStrength_MergeSort(lArr, cardsStrength);
+    SortByHandStrength_MergeSort(rArr, cardsStrength);
 
-    Merge(lArr, rArr, arr);
+    Merge(lArr, rArr, arr, cardsStrength);
 end
 
-
+-- endregion
 
 function daySolution.GetSolution()
     local solution = solutionCreatorModule.CreateSolution();
@@ -244,25 +292,79 @@ function daySolution.GetSolution()
                 tonumber(string.sub(line, spaceIdx + 1, #line)),
             }
 
-            local hand = Hand:New();
+            local hand = Hand:New(false);
             hand:InitializeHand(parts[1]);
             hand:InitializeBid(parts[2]);
             hands[#hands + 1] = hand;
         end
 
-        --SortHandByStrength_BubbleSort(hands);
-        --SortByHandStrength_QuickSort(hands, 1, #hands);
-        SortByHandStrength_MergeSort(hands);
+        --SortHandByStrength_BubbleSort(hands, CardsStrengthPT1);
+        --SortByHandStrength_QuickSort(hands, 1, #hands, CardsStrengthPT1);
+        SortByHandStrength_MergeSort(hands,
+            {
+                ['A'] = 13,
+                ['K'] = 12,
+                ['Q'] = 11,
+                ['J'] = 10,
+                ['T'] = 9,
+                ['9'] = 8,
+                ['8'] = 7,
+                ['7'] = 6,
+                ['6'] = 5,
+                ['5'] = 4,
+                ['4'] = 3,
+                ['3'] = 2,
+                ['2'] = 1
+            });
+
         local sum = 0;
         for i = 1, #hands, 1 do
-            --sum = sum + hands[i].bidAmount * ((#hands - i) + 1);
             sum = sum + hands[i].bidAmount * i;
         end
 
         return sum;
     end
     solution.Part2 = function(data)
-        return 0;
+        local hands = {};
+
+        for line in string.gmatch(data, "[^\r\n]+") do
+            local spaceIdx = string.find(line, "%s");
+            local parts = {
+                string.sub(line, 1, spaceIdx - 1),
+                tonumber(string.sub(line, spaceIdx + 1, #line)),
+            }
+
+            local hand = Hand:New(true);
+            hand:InitializeHand(parts[1]);
+            hand:InitializeBid(parts[2]);
+            hands[#hands + 1] = hand;
+        end
+
+        --SortHandByStrength_BubbleSort(hands, CardsStrengthPT2);
+        --SortByHandStrength_QuickSort(hands, 1, #hands, CardsStrengthPT2);
+        SortByHandStrength_MergeSort(hands,
+            {
+                ['A'] = 13,
+                ['K'] = 12,
+                ['Q'] = 11,
+                ['T'] = 10,
+                ['9'] = 9,
+                ['8'] = 8,
+                ['7'] = 7,
+                ['6'] = 6,
+                ['5'] = 5,
+                ['4'] = 4,
+                ['3'] = 3,
+                ['2'] = 2,
+                ['J'] = 1
+            });
+
+        local sum = 0;
+        for i = 1, #hands, 1 do
+            sum = sum + hands[i].bidAmount * i;
+        end
+
+        return sum;
     end
 
     return solution;
